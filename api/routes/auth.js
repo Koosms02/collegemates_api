64f0dcd9e2b76beebb1acfb9c9cb4123ccdc5ddf
Koosms02 <Koosms02@gmail.com>
2023-09-26 18:23:42 +0200

    auth.js

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,12 +1,40 @@
 const express = require("express")
 const router = express.Router()
+const twilio = require("twilio")
+require('dotenv').config();
 //middleware to check if the use is logged in
 
+
+console.log(process.env.TWILIO_ACCOUNT_SID)
+const twilioClient = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN)
+const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER
+
+
+//function for generating the digits
+function generateVerificationCode() {
+    return Math.floor(100000 + Math.random() * 900000).toString();
+  }
+  
 //inputing phone number
 router.post("/phonenumber",async (req, res) => {
     const phonenumber = req.body.phonenumber
 
     //send the otp using libraries like fast2send and twilio
+    const finalOTP = generateVerificationCode();
+    twilioClient.messages.create({
+        body: `hi , here is your otp ${finalOTP}`,
+        to : phonenumber,
+        from: twilioPhoneNumber
+    }).then(messages => {
+        console.log(messages)
+    }).catch(error => {
+        console.log(error + "while sending the OTP")
+    })
+
+    res.status(200).json({
+        "phonenumber": phonenumber,
+        "message":"OPT sent!"
+    })
     
 })
 
