268d51eb219bb8e76ef18b328aed8b47f0a9ea77
Koosms02 <Koosms02@gmail.com>
2023-10-02 18:35:23 +0200

    user.js

--- api/models/user.js
+++ api/models/user.js
@@ -3,7 +3,27 @@ const mongoose = require("mongoose")
 
 const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    phonenumber: String
+    phonenumber: String,
+    name: String,
+    birthday: String,
+    age: Number,
+    university: String,
+    course: String,
+    residence: String,
+    location: Geolocation,
+    verified: String,
+    Photos: Array,
+    interest: Array,
+    bio: String,
+    gender: String,
+    preference: String,
+    status: String,
+    preferredDistance: Number,
+    preferredAgeRange: Array,
+    likedIds: Array,
+    disLikedIds: Array,
+    likedByIds: Array,
+    locality: String,
 })
 
 module.exports = mongoose.model('User', userSchema)
