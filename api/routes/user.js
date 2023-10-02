3c145c778a2ca92851c45c22bf44256dc831898d
Koosms02 <Koosms02@gmail.com>
2023-10-02 18:52:12 +0200

    user.js

--- api/routes/user.js
+++ api/routes/user.js
@@ -17,25 +17,47 @@ router.get("/", (req, res, next) => {
 
 //creating a user
 
-router.post("/", (req, res, next) => {
+router.post("/:userid", (req, res, next) => {
+
+
 
     const user = new User({
         _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
-        age: req.body.age
+        age: req.body.age,
+        name: req.body.name,
+        birthday: req.body.birthday,
+        age: req.body.age,
+        university: req.body, university,
+        course: req.body.course,
+        residence: req.body.residence,
+        location: req.body.location,
+        verified: req.body.verified,
+        Photos: req.body.Photos,
+        interest: req.body.interest,
+        bio: req.body.bio,
+        gender: req.body.gender,
+        preference: req.body.preference,
+        status: req.body.status,
+        preferredDistance: req.body.preferredDistance,
+        preferredAgeRange: req.body.preferredAgeRange,
+        likedIds: req.body.likedByIds,
+        disLikedIds: req.body.disLikedIds,
+        likedByIds: req.body.likedByIds,
+        locality: req.body.locality,
     })
 
-    console.log(user)
+    // console.log(user)
 
-    user.save().then(result => {
-        console.log(result)
-    }).catch(err => console.log("error:" + err))
+    // user.save().then(result => {
+    //     console.log(result)
+    // }).catch(err => console.log("error:" + err))
 
 
-    res.status(200).json({
-        message: "handling get request from user",
-        userInfo: user
-    })
+    // res.status(200).json({
+    //     message: "handling get request from user",
+    //     // userInfo: user
+    // })
 })
 
 module.exports = router
