734ef267b7250851fee3a411c0a47345cc7e3e6f
Koosms02 <Koosms02@gmail.com>
2023-10-03 19:27:43 +0200

    user.js

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,6 +2,8 @@ const express = require("express")
 const router = express.Router()
 const mongoose = require("mongoose")
 
+const ObjectId = mongoose.ObjectId
+
 
 const User = require("../models/user")
 
@@ -17,40 +19,42 @@ router.get("/", (req, res, next) => {
 
 //creating a user
 
-router.post("/", (req, res, next) => {
+router.patch("/:userid", (req, res, next) => {
+    User.updateOne({ "_id": req.params.userid })
 
-    const user = new User({
-        _id: new mongoose.Types.ObjectId(),
-        // _id: req.body.id,
-        name: req.body.name,
-        age: req.body.age,
-        name: req.body.name,
-        birthday: req.body.birthday,
-        age: req.body.age,
-        university: req.body.university,
-        course: req.body.course,
-        residence: req.body.residence,
-        location: req.body.location,
-        verified: req.body.verified,
-        Photos: req.body.Photos,
-        interest: req.body.interest,
-        bio: req.body.bio,
-        gender: req.body.gender,
-        preference: req.body.preference,
-        status: req.body.status,
-        preferredDistance: req.body.preferredDistance,
-        preferredAgeRange: req.body.preferredAgeRange,
-        likedIds: req.body.likedByIds,
-        disLikedIds: req.body.disLikedIds,
-        likedByIds: req.body.likedByIds,
-        locality: req.body.locality,
-    })
+    // .then((snapshot) => res.status(200)
+    //     .json({ "user": snapshot }))
+    // const user = new User({
+    //     _id: req.body.id != null ? new mongoose.Types.ObjectId() : ObjectId(req.body.id),
+    //     name: req.body.name,
+    //     age: req.body.age,
+    //     name: req.body.name,
+    //     birthday: req.body.birthday,
+    //     age: req.body.age,
+    //     university: req.body.university,
+    //     course: req.body.course,
+    //     residence: req.body.residence,
+    //     location: req.body.location,
+    //     verified: req.body.verified,
+    //     Photos: req.body.Photos,
+    //     interest: req.body.interest,
+    //     bio: req.body.bio,
+    //     gender: req.body.gender,
+    //     preference: req.body.preference,
+    //     status: req.body.status,
+    //     preferredDistance: req.body.preferredDistance,
+    //     preferredAgeRange: req.body.preferredAgeRange,
+    //     likedIds: req.body.likedByIds,
+    //     disLikedIds: req.body.disLikedIds,
+    //     likedByIds: req.body.likedByIds,
+    //     locality: req.body.locality,
+    // })
 
     // console.log(user)
 
-    user.save().then(result => {
-        res.status(200).json({ "message": "data_saved" })
-    }).catch(err => res.status(500).json({ "error": err }))
+    // user.save().then(result => {
+    //     res.status(200).json({ "message": "data_saved" })
+    // }).catch(err => res.status(500).json({ "error": err }))
 
 })
 
